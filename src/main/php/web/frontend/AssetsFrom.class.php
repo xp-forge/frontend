ef0fe4320c56453ab6aab553f6a0ac74c2c7b525
Timm Friebe <thekid@thekid.de>
2021-03-27 19:56:05 +0100

    Add handler to serve frontend assets
    Basically the same as web.handlers.FilesFrom, but with support for compressed
    files (`Accept-Encoding: gzip` will also check for [file].gz) and a default
    caching strategy

---
+++ src/main/php/web/frontend/AssetsFrom.class.php
@@ -0,0 +1,112 @@
+<?php namespace web\frontend;
+
+use io\Path;
+use util\MimeType;
+use web\{Handler, Headers};
+
+/**
+ * Serves assets from a given path. Checks for files with extensions matching
+ * the encodings passed in the `Accept-Encoding` header before falling back to
+ * the original file name.
+ *
+ * @test web.frontend.unittest.AssetsFromTest
+ * @see  https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Encoding
+ */
+class AssetsFrom implements Handler {
+  const EXTENSIONS = [
+    'br'       => '.br',
+    'gzip'     => '.gz',
+    'deflate'  => '.dfl',
+    'bzip2'    => '.bz2',
+    'identity' => '',
+    '*'        => ''
+  ];
+
+  private $path;
+
+  /**
+   * Instantiate an asset handler. Serves assets from the given path, using
+   * a given maximum age (in seconds) for the cache control header.
+   *
+   * @param  io.Path|io.Folder|string $path
+   * @param  int $cacheFor
+   */
+  public function __construct($path, $cacheFor= 2419200) {
+    $this->path= $path instanceof Path ? $path : new Path($path);
+    $this->cacheFor= $cacheFor;
+  }
+
+  /**
+   * Returns encodings accepted by the client ordered by given qvalues.
+   * Guarantees a "*" value to exist, which selects the uncompressed file.
+   *
+   * @param  string $header
+   * @return [:float]
+   * @see    https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Encoding
+   */
+  public static function accepted($header) {
+    $r= [];
+    $o= 0;
+    $s= 1.0;
+    while ($o < strlen($header)) {
+      $o+= ' ' === $header[$o];
+      $p= strcspn($header, ',;', $o);
+      $value= substr($header, $o, $p);
+      $o+= $p;
+
+      if (';' === ($header[$o] ?? null)) {
+        $p= strcspn($header, ',', $o);
+        sscanf(substr($header, $o + 1, $p - 1), 'q=%f', $q);
+        $o+= $p;
+      } else {
+        $q= $s-= 0.01;
+      }
+      $o++;
+      $r[$value]= $q;
+    }
+
+    $r+= ['*' => 0.01];
+    arsort($r, SORT_NUMERIC);
+    return $r;
+  }
+
+  /**
+   * Handling implementation, serves files including handling of conditional
+   * `If-Modified-Since` logic.
+   *
+   * @param  web.Request $request
+   * @param  web.Response $response
+   * @return var
+   */
+  public function handle($request, $response) {
+    $path= $request->uri()->path();
+    $file= null;
+    foreach (self::accepted($request->header('Accept-Encoding', '')) as $encoding => $q) {
+      $target= new Path($this->path, $path.(self::EXTENSIONS[$encoding] ?? '*'));
+      if ($target->exists()) {
+        $file= $target->asFile();
+        '*' === $encoding || $response->header('Content-Encoding', $encoding);
+        break;
+      }
+    }
+
+    if (null === $file) {
+      $response->answer(404, 'Not Found');
+      $response->send('The asset \''.$path.'\' was not found', 'text/plain');
+      return;
+    }
+
+    $modified= $file->lastModified();
+    if (($conditional= $request->header('If-Modified-Since')) && $modified <= strtotime($conditional)) {
+      $response->answer(304, 'Not Modified');
+      $response->flush();
+      return;
+    }
+
+    $response->answer(200, 'OK');
+    $response->header('Last-Modified', gmdate('D, d M Y H:i:s T', $modified));
+    $response->header('X-Content-Type-Options', 'nosniff');
+    $response->header('Cache-Control', 'max-age='.$this->cacheFor.', must-revalidate');
+    $response->transfer($file->in(), MimeType::getByFileName($path), $file->size());
+  }
+}
---
+++ src/test/php/web/frontend/unittest/AssetsFromTest.class.php
@@ -0,0 +1,180 @@
+<?php namespace web\frontend\unittest;
+
+use io\{Files, File, Folder};
+use lang\Environment;
+use unittest\{After, Assert, Test, Values};
+use web\frontend\AssetsFrom;
+use web\io\{TestInput, TestOutput};
+use web\{Request, Response};
+
+class AssetsFromTest {
+  const CONTENTS = 'body { color: red; }';
+  const COMPRESSED = "^_\x8B>^H^\x002d_...";
+
+  private $remove= [];
+
+  /**
+   * Creates a temporary folder
+   *
+   * @param  [:string] $files
+   * @return io.Folder
+   */
+  private function folderWith($files) {
+    $f= new Folder(Environment::tempDir(), uniqid());
+    $f->create();
+    foreach ($files as $name => $content) {
+      Files::write(new File($f, $name), $content);
+    }
+    return $this->remove[]= $f;
+  }
+
+  /**
+   * Serve a GET request from the specified files
+   *
+   * @param  [:string] $files
+   * @param  string $path
+   * @param  [:string] $headers
+   * @return web.Response
+   */
+  private function serve($files, $path, $headers= []) {
+    $req= new Request(new TestInput('GET', $path, $headers));
+    $res= new Response(new TestOutput());
+
+    (new AssetsFrom($this->folderWith($files)))->handle($req, $res);
+    return $res;
+  }
+
+  /**
+   * Assertion helper
+   *
+   * @param  string $bytes
+   * @param  web.Response $res
+   * @throws unittest.AssertionFailedError
+   * @return void
+   */
+  private function assertFile($bytes, $res) {
+    Assert::equals("\r\n\r\n$bytes", strstr($res->output()->bytes(), "\r\n\r\n"));
+  }
+
+  #[Test]
+  public function can_create() {
+    new AssetsFrom('.');
+  }
+
+  #[Test]
+  public function can_create_with_folder() {
+    new AssetsFrom(new Folder('.'));
+  }
+
+  #[Test]
+  public function typical_ua_header_accepted() {
+    Assert::equals(
+      ['gzip' => 0.99, 'deflate' => 0.98, 'br' => 0.97, '*' => 0.01],
+      AssetsFrom::accepted('gzip, deflate, br')
+    );
+  }
+
+  #[Test]
+  public function identity_accepted() {
+    Assert::equals(
+      ['identity' => 0.99, '*' => 0.01],
+      AssetsFrom::accepted('identity')
+    );
+  }
+
+  #[Test]
+  public function header_with_qvalues_accepted() {
+    Assert::equals(
+      ['gzip' => 1.0, 'deflate' => 0.99, '*' => 0.5],
+      AssetsFrom::accepted('deflate, gzip;q=1.0, *;q=0.5')
+    );
+  }
+
+  #[Test, Values([null, 'deflate', 'gzip, deflate'])]
+  public function directly_serves_file($for) {
+    $files= ['fixture.css' => self::CONTENTS];
+    $res= $this->serve($files, '/fixture.css', ['Accept-Encoding' => $for]);
+
+    Assert::equals(200, $res->status());
+    Assert::equals('text/css', $res->headers()['Content-Type']);
+    Assert::false(isset($res->headers()['Content-Encoding']));
+    $this->assertFile($files['fixture.css'], $res);
+  }
+
+  #[Test, Values([[['fixture.css' => self::CONTENTS]], [['fixture.css.gz' => self::COMPRESSED]]])]
+  public function handles_conditional_requests($files) {
+    $res= $this->serve($files, '/fixture.css', [
+      'Accept-Encoding'   => 'gzip, deflate, br',
+      'If-Modified-Since' => gmdate('D, d M Y H:i:s T', time() + 86400)
+    ]);
+
+    Assert::equals(304, $res->status());
+  }
+
+  #[Test]
+  public function returns_error_when_file_is_not_found() {
+    $files= ['fixture.css' => self::CONTENTS];
+    $res= $this->serve($files, '/nonexistant.css');
+
+    Assert::equals(404, $res->status());
+  }
+
+  #[Test, Values([['fixture.css.gz', 'gzip'], ['fixture.css.br', 'br'], ['fixture.css.dfl', 'deflate'], ['fixture.css.bz2', 'bzip2']])]
+  public function serves_compressed_when_gz_file_present($file, $encoding) {
+    $files= [$file => self::COMPRESSED];
+    $res= $this->serve($files, '/fixture.css', ['Accept-Encoding' => 'gzip, bzip2, deflate, br']);
+
+    Assert::equals(200, $res->status());
+    Assert::equals('text/css', $res->headers()['Content-Type']);
+    Assert::equals($encoding, $res->headers()['Content-Encoding']);
+    $this->assertFile($files[$file], $res);
+  }
+
+  #[Test]
+  public function prefers_gzip_compressed_when_gz_file_present() {
+    $files= ['fixture.css' => self::CONTENTS, 'fixture.css.gz' => self::COMPRESSED];
+    $res= $this->serve($files, '/fixture.css', ['Accept-Encoding' => 'gzip, br']);
+
+    Assert::equals(200, $res->status());
+    Assert::equals('text/css', $res->headers()['Content-Type']);
+    Assert::equals('gzip', $res->headers()['Content-Encoding']);
+    $this->assertFile($files['fixture.css.gz'], $res);
+  }
+
+  #[Test]
+  public function prefers_uncompressed_for_identity() {
+    $files= ['fixture.css' => self::CONTENTS, 'fixture.css.gz' => self::COMPRESSED];
+    $res= $this->serve($files, '/fixture.css', ['Accept-Encoding' => 'identity;q=1.0, gzip']);
+
+    Assert::equals(200, $res->status());
+    Assert::equals('text/css', $res->headers()['Content-Type']);
+    Assert::equals('identity', $res->headers()['Content-Encoding']);
+    $this->assertFile($files['fixture.css'], $res);
+  }
+
+  #[Test, Values([null, 'deflate', 'br, deflate;q=0.5', 'test'])]
+  public function prefers_uncompressed_without_browser_support($for) {
+    $files= ['fixture.css' => self::CONTENTS, 'fixture.css.gz' => self::COMPRESSED];
+    $res= $this->serve($files, '/fixture.css', ['Accept-Encoding' => $for]);
+
+    Assert::equals(200, $res->status());
+    Assert::equals('text/css', $res->headers()['Content-Type']);
+    Assert::false(isset($res->headers()['Content-Encoding']));
+    $this->assertFile($files['fixture.css'], $res);
+  }
+
+  #[Test, Values([null, 'deflate', 'br, deflate;q=0.5', 'test'])]
+  public function returns_error_without_browser_support($for) {
+    $files= ['fixture.css.gz' => self::COMPRESSED];
+    $res= $this->serve($files, '/fixture.css', ['Accept-Encoding' => $for]);
+
+    Assert::equals(404, $res->status());
+  }
+
+  #[After]
+  public function cleanup() {
+    foreach ($this->remove as $folder) {
+      $folder->unlink();
+    }
+  }
+}
