2ae837a3e6a4bf534d0756635ea11690f8736561
Timm Friebe <thekid@thekid.de>
2021-04-03 21:28:59 +0200

    Add AssetsManifest::immutable() and tests

--- src/main/php/web/frontend/AssetsManifest.class.php
+++ src/main/php/web/frontend/AssetsManifest.class.php
@@ -2,11 +2,32 @@
 
 use text\json\{Json, Input, FileInput};
 
+/**
+ * Assets manifest 
+ *
+ * @see   https://webpack.js.org/concepts/manifest/
+ * @test  web.frontend.unittest.AssetsManifestTest
+ */
 class AssetsManifest {
   public $assets;
 
   /** @param text.json.Input|io.Path|io.File|string */
   public function __construct($arg) {
     $this->assets= Json::read($arg instanceof Input ? $arg : new FileInput($arg));
+  }
+
+  /**
+   * Returns an immutable cache header if a file is contained in this
+   * manifest.
+   *
+   * @param  io.Path|io.File|string $file
+   * @return ?string
+   */
+  public function immutable($file) {
+    $compare= $file instanceof File ? $file->filename : (string)$file;
+    foreach ($this->assets as $resolved) {
+      if (0 === strpos($compare, $resolved)) return 'max-age=31536000, immutable';
+    }
+    return null;
   }
 }
---
+++ src/test/php/web/frontend/unittest/AssetsManifestTest.class.php
@@ -0,0 +1,64 @@
+<?php namespace web\frontend\unittest;
+
+use lang\FormatException;
+use text\json\StringInput;
+use unittest\{Test, Values, Expect, TestCase};
+use web\frontend\AssetsManifest;
+
+class AssetsManifestTest extends TestCase {
+
+  /** @return iterable */
+  private function inputs() {
+    yield ['{}'];
+    yield ['{"vendor.css" : "vendor.f6cad2a.css"}'];
+    yield ['{"vendor.css" : "vendor.f6cad2a.css", "vendor.js" : "vendor.7b1f7cb.js"}'];
+  }
+
+  /**
+   * Creates a new fixture
+   *
+   * @param  string $input
+   * @return web.frontend.AssetsManifest
+   */
+  private function fixture($input) {
+    return new AssetsManifest(new StringInput($input));
+  }
+
+  #[Test]
+  public function can_create() {
+    $this->fixture('{}');
+  }
+
+  #[Test, Expect(class: FormatException::class, withMessage: '/Unexpected token/')]
+  public function cannot_create_with_malformed() {
+    $this->fixture('not.json');
+  }
+
+  #[Test, Values('inputs')]
+  public function assets($input) {
+    $this->assertEquals(json_decode($input, true), $this->fixture($input)->assets);
+  }
+
+  #[Test]
+  public function immutable_asset() {
+    $this->assertEquals(
+      'max-age=31536000, immutable',
+      $this->fixture('{"vendor.css" : "vendor.f6cad2a.css"}')->immutable('vendor.f6cad2a.css')
+    );
+  }
+
+  #[Test]
+  public function immutable_gzipped_asset() {
+    $this->assertEquals(
+      'max-age=31536000, immutable',
+      $this->fixture('{"vendor.css" : "vendor.f6cad2a.css"}')->immutable('vendor.f6cad2a.css.gz')
+    );
+  }
+
+  #[Test]
+  public function regular_asset() {
+    $this->assertNull(
+      $this->fixture('{"vendor.css" : "vendor.f6cad2a.css"}')->immutable('style.css')
+    );
+  }
+}
