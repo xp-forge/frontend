b1b234759d67e267b67db2609505ad6deafc5562
Timm Friebe <thekid@thekid.de>
2021-05-15 13:50:20 +0200

    Add web.frontend.Exceptions class to map exceptions to handlers

---
+++ src/main/php/web/frontend/Exceptions.class.php
@@ -0,0 +1,62 @@
+<?php namespace web\frontend;
+
+use lang\IllegalArgumentException;
+use web\Error;
+
+/**
+ * Maps exceptions to views
+ *
+ * ```php
+ * $handler= new Exceptions();
+ *
+ * // Use errors/{status} for web.Error instances, errors/500 for others
+ * $handler->mapping(Throwable::class);
+ *
+ * // Always use errors/503 for all SQLExceptions
+ * $handler->mapping(SQLException::class, 503);
+ *
+ * // Supply a handler function returning a view
+ * $handler->mapping(InvalidOrder::class, fn($e) => View::error(404, 'invalid-order'));
+ * ```
+ *
+ * @test  web.frontend.unittest.ExceptionsTest
+ */
+class Exceptions implements Errors {
+  private $mapping= [];
+
+  /**
+   * Maps an exception type to a handler
+   *
+   * @param  string $type
+   * @param  ?int|callable $handler
+   * @return self
+   */
+  public function mapping($type, $handler= null) {
+    if (null === $handler) {
+      $this->mapping[$type]= function($cause) { return View::error($cause instanceof Error ? $cause->status() : 500); };
+    } else if (is_int($handler)) {
+      $this->mapping[$type]= function() use($handler) { return View::error($handler); };
+    } else if (is_callable($handler)) {
+      $this->mapping[$type]= $handler;
+    } else {
+      throw new IllegalArgumentException('Expected NULL, an integer or a callable');
+    }
+    return $this;
+  }
+
+  /**
+   * Handles errors
+   *
+   * @param  lang.Throwable $cause
+   * @return web.frontend.View
+   * @throws web.Error
+   */
+  public function handle($cause) {
+    foreach ($this->mapping as $type => $mapping) {
+      if ($cause instanceof $type) return $mapping($cause)->with(['cause' => $cause]);
+    }
+
+    // Unhanded, raise error and display default error page
+    throw $cause instanceof Error ? $cause : new Error(500, $cause->getMessage(), $cause);
+  }
+}
---
+++ src/test/php/web/frontend/unittest/ExceptionsTest.class.php
@@ -0,0 +1,111 @@
+<?php namespace web\frontend\unittest;
+
+use lang\{IllegalArgumentException, Throwable};
+use unittest\{Assert, Expect, Test};
+use web\Error;
+use web\frontend\{Exceptions, View};
+
+class ExceptionsTest {
+
+  #[Test]
+  public function can_create() {
+    new Exceptions();
+  }
+
+  #[Test, Expect(class: Error::class, withMessage: 'test')]
+  public function throws_error_if_not_mapped() {
+    (new Exceptions())->handle(new Error(404, 'test'));
+  }
+
+  #[Test, Expect(class: Error::class, withMessage: 'test')]
+  public function throws_as_error_if_not_mapped() {
+    (new Exceptions())->handle(new IllegalArgumentException('test'));
+  }
+
+  #[Test]
+  public function mapping_web_error_uses_its_statuscode() {
+    $view= (new Exceptions())
+      ->mapping(Error::class)
+      ->handle(new Error(404, 'test'))
+    ;
+    Assert::equals([404, 'errors/404'], [$view->status, $view->template]);
+  }
+
+  #[Test]
+  public function mapping_exception_uses_500_as_statuscode() {
+    $view= (new Exceptions())
+      ->mapping(IllegalArgumentException::class)
+      ->handle(new IllegalArgumentException('test'))
+    ;
+    Assert::equals([500, 'errors/500'], [$view->status, $view->template]);
+  }
+
+  #[Test]
+  public function mapping_exception_to_statuscode() {
+    $view= (new Exceptions())
+      ->mapping(IllegalArgumentException::class, 400)
+      ->handle(new IllegalArgumentException('test'))
+    ;
+    Assert::equals([400, 'errors/400'], [$view->status, $view->template]);
+  }
+
+  #[Test]
+  public function mapping_exception_to_callable() {
+    $view= (new Exceptions())
+      ->mapping(IllegalArgumentException::class, function($e) { return View::error(400, 'validation'); })
+      ->handle(new IllegalArgumentException('test'))
+    ;
+    Assert::equals([400, 'errors/validation'], [$view->status, $view->template]);
+  }
+
+  #[Test]
+  public function mapping_throwable_to_catch_all() {
+    $view= (new Exceptions())
+      ->mapping(Throwable::class)
+      ->handle(new IllegalArgumentException('test'))
+    ;
+    Assert::equals([500, 'errors/500'], [$view->status, $view->template]);
+  }
+
+  #[Test]
+  public function order_is_relevant() {
+    $view= (new Exceptions())
+      ->mapping(IllegalArgumentException::class, 400)
+      ->mapping(Throwable::class)
+      ->handle(new IllegalArgumentException('test'))
+    ;
+    Assert::equals([400, 'errors/400'], [$view->status, $view->template]);
+  }
+
+  #[Test]
+  public function parent_shadows_type() {
+    $view= (new Exceptions())
+      ->mapping(Throwable::class)
+      ->mapping(IllegalArgumentException::class, 400) // Not used in this case!
+      ->handle(new IllegalArgumentException('test'))
+    ;
+    Assert::equals([500, 'errors/500'], [$view->status, $view->template]);
+  }
+
+  #[Test]
+  public function context_contains_exception() {
+    $view= (new Exceptions())
+      ->mapping(IllegalArgumentException::class)
+      ->handle(new IllegalArgumentException('test'))
+    ;
+
+    Assert::instance(IllegalArgumentException::class, $view->context['cause']);
+  }
+
+  #[Test]
+  public function context_from_callables_is_merged() {
+    $handler= function($e) { return View::error(400, 'validation')->with(['type' => get_class($e)]); };
+    $view= (new Exceptions())
+      ->mapping(IllegalArgumentException::class, $handler)
+      ->handle(new IllegalArgumentException('test'))
+    ;
+
+    Assert::equals(IllegalArgumentException::class, $view->context['type']);
+    Assert::instance(IllegalArgumentException::class, $view->context['cause']);
+  }
+}
