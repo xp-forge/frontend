7547e5e427660601727e8c715d0b34bc2fbe6e97
Timm Friebe <thekid@thekid.de>
2021-05-15 13:55:50 +0200

    Test handling() and errors() methods

--- src/test/php/web/frontend/unittest/FrontendTest.class.php
+++ src/test/php/web/frontend/unittest/FrontendTest.class.php
@@ -1,15 +1,15 @@
 <?php namespace web\frontend\unittest;
 
 use lang\IllegalArgumentException;
-use unittest\{Expect, Test, TestCase};
+use unittest\{Assert, Before, Expect, Test};
 use web\frontend\unittest\actions\Users;
-use web\frontend\{Frontend, Templates};
+use web\frontend\{Frontend, Exceptions, RaiseErrors, Templates};
 
-class FrontendTest extends TestCase {
+class FrontendTest {
   private $templates;
 
-  /** @return void */
-  public function setUp() {
+  #[Before]
+  public function templates() {
     $this->templates= new class() implements Templates {
       public function write($template, $context, $out) { /* NOOP */ }
     };
@@ -27,17 +27,28 @@ class FrontendTest extends TestCase {
 
   #[Test]
   public function globals_empty_by_default() {
-    $this->assertEquals([], (new Frontend(new Users(), $this->templates))->globals);
+    Assert::equals([], (new Frontend(new Users(), $this->templates))->globals);
   }
 
   #[Test]
   public function globals_passed_to_constructor() {
     $globals= ['base' => '/', 'fingerprint' => '99b3825'];
-    $this->assertEquals($globals, (new Frontend(new Users(), $this->templates, $globals))->globals);
+    Assert::equals($globals, (new Frontend(new Users(), $this->templates, $globals))->globals);
   }
 
   #[Test]
   public function base_passed_to_constructor() {
-    $this->assertEquals(['base' => ''], (new Frontend(new Users(), $this->templates, '/'))->globals);
+    Assert::equals(['base' => ''], (new Frontend(new Users(), $this->templates, '/'))->globals);
+  }
+
+  #[Test]
+  public function raises_errors_by_default() {
+    Assert::instance(RaiseErrors::class, (new Frontend(new Users(), $this->templates))->errors());
+  }
+
+  #[Test]
+  public function changed_exception_handling() {
+    $h= new Exceptions();
+    Assert::equals($h, (new Frontend(new Users(), $this->templates))->handling($h)->errors());
   }
 }
