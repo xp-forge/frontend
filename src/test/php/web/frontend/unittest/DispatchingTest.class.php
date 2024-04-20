38afb6278feda58dae506c69b476c2535c56071c
Timm Friebe <thekid@thekid.de>
2024-04-20 18:41:30 +0200

    Test dispatching outside of frontend

--- src/test/php/web/frontend/unittest/DispatchingTest.class.php
+++ src/test/php/web/frontend/unittest/DispatchingTest.class.php
@@ -2,9 +2,9 @@
 
 use test\{Assert, Test};
 use util\URI;
-use web\frontend\{Frontend, Templates, View};
+use web\frontend\{Frontend, Templates, Get, Param, View};
 use web\io\{TestInput, TestOutput};
-use web\{Request, Response};
+use web\{Application, Environment, Request, Response};
 
 class DispatchingTest {
   private $templates;
@@ -15,40 +15,81 @@ class DispatchingTest {
    * @param  object $handler
    * @param  string $method
    * @param  string $uri
-   * @return ?iterable
+   * @return string
    */
   private function handle($handler, $method, $uri) {
     $req= new Request(new TestInput($method, $uri));
-    $res= new Response(new TestOutput());
-    $templates= new class() implements Templates {
-      public function write($template, $context, $out) { /* NOOP */ }
+    $res= new Response((new TestOutput())->buffered());
+    $app= new class(new Environment('test'), $handler) extends Application {
+      private $handler;
+
+      public function __construct(Environment $environment, $handler) {
+        parent::__construct($environment);
+        $this->handler= $handler;
+      }
+
+      public function routes() {
+        return [
+          '/static' => function($req, $res) {
+            $res->send("Static {$req->uri()->path()}", 'text/plain');
+          },
+          '/'       => new Frontend($this->handler, new class() implements Templates {
+            public function write($template, $context, $out) {
+              $out->write("Template {$template}");
+            }
+          }),
+        ];
+      }
     };
 
-    return (new Frontend($handler, $templates))->handle($req, $res);
+    foreach ($app->service($req, $res) ?? [] as $_) { }
+    return $res->output()->body();
   }
 
   #[Test]
-  public function dispatch() {
+  public function to_route() {
     $fixture= new class() {
+      #[Get]
+      public function dispatcher() {
+        return View::dispatch('/static/file');
+      }
+    };
+
+    Assert::equals('Static /static/file', $this->handle($fixture, 'GET', '/'));
+  }
+
+  #[Test]
+  public function internally() {
+    $fixture= new class() {
+      #[Get('/users')]
+      public function users() {
+        return View::named('users');
+      }
+
       #[Get]
       public function dispatcher() {
         return View::dispatch('/users');
       }
     };
 
-    Assert::equals(new URI('http://localhost/users'), $this->handle($fixture, 'GET', '/')->uri());
+    Assert::equals('Template users', $this->handle($fixture, 'GET', '/'));
   }
 
   #[Test]
   public function with_params() {
     $fixture= new class() {
+      #[Get('/users')]
+      public function users(#[Param] string $test) {
+        return View::named("users-{$test}");
+      }
+
       #[Get]
       public function dispatcher() {
         return View::dispatch('/users', ['test' => 'success']);
       }
     };
 
-    Assert::equals(new URI('http://localhost/users?test=success'), $this->handle($fixture, 'GET', '/')->uri());
+    Assert::equals('Template users-success', $this->handle($fixture, 'GET', '/'));
   }
 
   #[Test]
@@ -56,10 +97,10 @@ class DispatchingTest {
     $fixture= new class() {
       #[Get]
       public function empty() {
-        // No implementation
+        return View::named('test');
       }
     };
 
-    Assert::null($this->handle($fixture, 'GET', '/'));
+    Assert::equals('Template test', $this->handle($fixture, 'GET', '/'));
   }
 }
